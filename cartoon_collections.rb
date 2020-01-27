47248ab1c298a4769d6934cd2a8c3fcdb7a71e77
Muhan Lee <davidmlee4@gmail.com>
2020-01-27 00:42:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-  
+  array.map {|n| "#{n.capitalize!}"}
 end
 
 def long_planeteer_calls(four_char)# code an argument here
